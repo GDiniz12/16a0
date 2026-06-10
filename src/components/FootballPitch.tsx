ff386ed3a1f881a0ab5e115846eae4a12360bba4
Gabriel Diniz <gabrielvmdiniz12@gmail.com>
2026-06-10 15:28:45 -0300

    Fix: football pitch positions

--- src/components/FootballPitch.tsx
+++ src/components/FootballPitch.tsx
@@ -16,9 +16,9 @@ export default function FootballPitch({
   highlightedSlots = [],
 }: FootballPitchProps) {
   return (
-    <div className="w-full max-w-[440px] mx-auto">
+    <div className="w-full max-w-[420px] mx-auto p-2">
       <div
-        className="relative w-full rounded-none border-4 border-white shadow-[12px_12px_0_0_rgba(0,0,0,0.7)] p-6"
+        className="relative w-full rounded-none border-4 border-white shadow-[12px_12px_0_0_rgba(0,0,0,0.7)]"
         style={{
           paddingBottom: "145%",
           background: "#1A3B2B",
@@ -28,7 +28,7 @@ export default function FootballPitch({
         <div className="absolute top-1/2 left-0 w-full h-1 bg-white/30 -translate-y-1/2 pointer-events-none" />
         
         {/* Losango Central Brutalista */}
-        <div className="absolute top-1/2 left-1/2 w-28 h-28 border-4 border-white/30 -translate-x-1/2 -translate-y-1/2 rounded-none transform rotate-45 pointer-events-none" />
+        <div className="absolute top-1/2 left-1/2 w-20 h-20 md:w-28 md:h-28 border-4 border-white/30 -translate-x-1/2 -translate-y-1/2 rounded-none transform rotate-45 pointer-events-none" />
 
         {/* Área Superior */}
         <div className="absolute top-0 left-1/2 w-[55%] h-[18%] border-b-4 border-l-4 border-r-4 border-white/30 -translate-x-1/2 pointer-events-none" />
